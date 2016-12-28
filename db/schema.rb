feb6df9620ab24833bc53372a5a69f53f4841faa
Aakash Gupta <aakash.gupta96@yahoo.in>
2016-12-28 14:37:00 +0530

    Steps updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161226104407) do
+ActiveRecord::Schema.define(version: 20161227090846) do
 
   create_table "compare_objects", force: :cascade do |t|
     t.string   "name"
@@ -33,5 +33,20 @@ ActiveRecord::Schema.define(version: 20161226104407) do
     t.integer  "comparisons"
     t.string   "video_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",              default: "", null: false
+    t.string   "encrypted_password", default: "", null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "provider"
+    t.string   "uid"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
