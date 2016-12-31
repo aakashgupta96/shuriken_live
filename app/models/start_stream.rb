29ed0b5fa22b79e6cfe7f2bf3383265cb47099ba
Aakash Gupta <aakash.gupta96@yahoo.in>
2016-12-31 11:52:08 +0530

    FFMPEG command changed

--- app/models/start_stream.rb
+++ app/models/start_stream.rb
@@ -4,6 +4,7 @@ class StartStream
 
 	def self.perform(post_id)
 		post = Post.find(post_id)
-		result = %x[ffmpeg -loop 1 -re -y -f image2 -i "public/uploads/post/#{post.id}/frame2.png" -i "public/silent.aac" -acodec libfdk_aac -ac 1 -ar 44100 -b:a 128k -pix_fmt yuv420p -profile:v baseline -s 1280x720 -bufsize 6000k -vb 400k -maxrate 1500k -deinterlace -vcodec libx264 -preset veryfast -t #{(post.duration-30.years).to_i + 50} -g 30 -r 30 -strict -2 -f flv "rtmp://rtmp-api.facebook.com:80/rtmp/#{post.key}"]
+		#result = %x[ffmpeg -loop 1 -re -y -f image2 -i "public/uploads/post/#{post.id}/frame2.png" -i "public/silent.aac" -acodec libfdk_aac -ac 1 -ar 44100 -b:a 128k -pix_fmt yuv420p -profile:v baseline -s 1280x720 -bufsize 6000k -vb 400k -maxrate 1500k -deinterlace -vcodec libx264 -preset veryfast -t #{(post.duration-30.years).to_i + 50} -g 30 -r 30 -strict -2 -f flv "rtmp://rtmp-api.facebook.com:80/rtmp/#{post.key}"]
+		result = %x[ffmpeg -loop 1 -re -y -f image2 -i "public/uploads/post/#{post.id}/frame2.png" -i "public/silent.aac" -codec:a aac -ac 1 -ar 44100 -b:a 128k -pix_fmt yuv420p -profile:v baseline -s 1280x720 -bufsize 6000k -vb 400k -maxrate 1500k -deinterlace -vcodec libx264 -preset veryfast -t #{(post.duration-30.years).to_i + 50} -g 30 -r 30 -strict -2 -f flv "rtmp://rtmp-api.facebook.com:80/rtmp/#{post.key}"]
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161230165742) do
+ActiveRecord::Schema.define(version: 20161227090846) do
 
   create_table "compare_objects", force: :cascade do |t|
     t.string   "name"
