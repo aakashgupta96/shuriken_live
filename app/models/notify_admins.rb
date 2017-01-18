fe79deacd0ae81253e522c0030dda5992cec1902
Aakash Gupta <aakash.gupta96@yahoo.in>
2017-01-18 21:33:33 +0530

    Admin emails added

--- app/models/notify_admins.rb
+++ app/models/notify_admins.rb
@@ -1,7 +1,6 @@
 class NotifyAdmins
 	@queue = :notify_admins
-	#, "apoorva.11596@gmail.com", "tushar@codingninjas.in"
-	@admins = ["aakash@shurikenlive.com"]
+	@admins = ["aakash@shurikenlive.com", "apoorva.11596@gmail.com", "tushar@codingninjas.in"]
 	def self.perform(post_id)
 		post = Post.find(post_id)
 		link = "www.facebook.com/#{post.video_id}"
