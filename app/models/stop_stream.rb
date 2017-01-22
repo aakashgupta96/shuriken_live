1a52c5569665023ca1e5b9cac10bc093619d62ab
Ubuntu <ubuntu@ip-172-31-14-228.us-west-1.compute.internal>
2017-01-22 21:59:13 +0000

    minor changes from prod

--- app/models/stop_stream.rb
+++ app/models/stop_stream.rb
@@ -22,7 +22,7 @@ class StopStream
 					post_id = StopStream.retrieve_post_id(worker)
 					p = Post.find(post_id)
 					duration = (p.duration-30.years).to_i + 50
-					puts "Duration is ",duration," and elapsed_time is ",elapsed_time
+				#	puts "Duration is ",duration," and elapsed_time is ",elapsed_time
 					if(elapsed_time >= duration)
 						%x[kill #{x}]	
 					end
