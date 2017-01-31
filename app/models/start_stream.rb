3be18dac0b027e7f101b0180f4a7b294ca4b16d9
Aakash Gupta <aakash.gupta96@yahoo.in>
2017-01-31 14:27:18 +0530

    Key frame rate fixed with increased max bit rate

--- app/models/start_stream.rb
+++ app/models/start_stream.rb
@@ -13,7 +13,7 @@ class StartStream
 			%x[$HOME/bin/ffmpeg -stream_loop 10000 -i "#{audio_path}/final.aac" -c copy -t 14400 "#{audio_path}/long.aac"]
 			audio_path = "public/uploads/post/#{post.id}/long.aac"
 		end
-		result = %x[$HOME/bin/ffmpeg -loop 1 -re -y -f image2 -i #{frame_path} -i #{audio_path} -acodec copy -bsf:a aac_adtstoasc -pix_fmt yuv420p -profile:v high -s 1280x720 -vb 400k -maxrate 400k -minrate 400k -bufsize 600k -deinterlace -vcodec libx264 -preset veryfast -g 30 -r 30 -t 14400 -strict -2 -f flv "rtmp://rtmp-api.facebook.com:80/rtmp/#{post.key}" 2> #{Rails.root.join("log").join("stream").join(post.id.to_s).to_s} ]
+		result = %x[$HOME/bin/ffmpeg -loop 1 -re -y -f image2 -i #{frame_path} -i #{audio_path} -acodec copy -bsf:a aac_adtstoasc -pix_fmt yuv420p -profile:v high -s 1280x720 -vb 400k -maxrate 1500k -minrate 400k -bufsize 6000k -deinterlace -vcodec libx264 -preset veryfast -g 60 -r 30 -t 14400 -strict -2 -f flv "rtmp://rtmp-api.facebook.com:80/rtmp/#{post.key}" 2> #{Rails.root.join("log").join("stream").join(post.id.to_s).to_s} ]
 		Resque.workers.each do |worker|
 			if worker.working? and worker.queues.first == "update_frame" and worker.job["payload"]["args"].first==post_id
 				victim = %x[pgrep -P #{worker.pid}]	
